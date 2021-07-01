1a61422ddc362cdb5087bcf821ddda49caab5e46
mounvip1525 <mounvip1525@gmail.com>
2021-07-01 15:16:30 +0530

    single color palette initial

--- palette/src/App.js
+++ palette/src/App.js
@@ -27,12 +27,16 @@ class App extends Component {
             />
           )}
         />
-        <Route exact path='/palette/:paletteId/:colorId' render={()=><SingleColorPalette />} />
+        <Route exact path='/palette/:paletteId/:colorId' 
+          render={routeProps => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}  /> 
       </Switch>
-
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
