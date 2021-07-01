2118ae0a30a80ba75be9faa5cca76fc2b6608d6b
mounvip1525 <mounvip1525@gmail.com>
2021-07-01 11:42:04 +0530

    select material component added

--- palette/src/containers/Pallete.js
+++ palette/src/containers/Pallete.js
@@ -8,21 +8,26 @@ export default class Pallete extends Component {
     super(props);
     this.state = {
       level: 400,
+      format:"hex"
     };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat=this.changeFormat.bind(this);
   }
   changeLevel(newLevel) {
     this.setState({ level: newLevel });
   }
+  changeFormat(val){
+      this.setState({format:val})
+  }
   render() {
     const { colors } = this.props.pallete;
-    const { level } = this.state;
+    const { level , format } = this.state;
     const colorBoxes = colors[level].map((color) => {
-      return <ColorBox background={color.hex} name={color.name} />;
+      return <ColorBox background={color[format]} name={color.name} />;
     });
     return (
       <div className="Pallete">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
         <div className="Pallete-colors">{colorBoxes}</div>
       </div>
     );
